1f6362a4c18cf1766162ea21104d27b5d8ed68a4
Jesus <jjsamerico@gmail.com>
2019-04-11 20:39:32 -0300

    Layout genérico da tela principal do totem

---
+++ app/src/main/java/com/voador/guardeiro/flightclub/TabletMainActivity.java
@@ -0,0 +1,13 @@
+package com.voador.guardeiro.flightclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabletMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablet_main);
+    }
+}
