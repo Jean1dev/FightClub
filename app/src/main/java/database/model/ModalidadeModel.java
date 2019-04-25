7f4cc59aed57501b40c2253a57b450adda70cded
rogervieiraa <rogeramorinvieiraa@gmail.com>
2019-04-25 20:42:34 -0300

    Dando nome, e criando tabela

--- app/src/main/java/database/model/ModalidadeModel.java
+++ app/src/main/java/database/model/ModalidadeModel.java
@@ -4,6 +4,17 @@ public class ModalidadeModel {
 
     private String modalidade;
 
+    //TABELA
+    public static final String TABELA_NOME = "modalidade",
+            COLUNA_ID = "_id",
+            COLUNA_MODALIDADE = "modalidade";
+
+    public static final String CREATE_TABLE = " create table " + TABELA_NOME
+            +"("
+            +COLUNA_ID +" integer primary key autoincrement,"
+            +COLUNA_MODALIDADE +" text "
+            + ")";
+
     public ModalidadeModel(String modalidade) {
         this.modalidade = modalidade;
     }
--- app/src/main/java/database/model/PlanosModel.java
+++ app/src/main/java/database/model/PlanosModel.java
@@ -5,6 +5,20 @@ public class PlanosModel {
     private ModalidadeModel modalidadeModel;
     private String plano;
 
+    //TABELA
+    public static final String TABELA_NOME = "planos",
+            COLUNA_ID = "_id",
+            COLUNA_I_MODALIDADE_MODEL = "i_modalidade_model",
+            COLUNA_PLANO = "plano";
+
+    public static final String CREATE_TABLE = " create table " + TABELA_NOME
+            +"("
+            +COLUNA_ID +" integer primary key autoincrement,"
+            +COLUNA_I_MODALIDADE_MODEL +" integer, "
+            +COLUNA_PLANO +" text, "
+            + "FOREIGN KEY (i_modalidade_model) REFERENCES 'modalidade' ('_id') "
+            + ")";
+
     public PlanosModel(ModalidadeModel modalidadeModel, String plano) {
         this.modalidadeModel = modalidadeModel;
         this.plano = plano;
