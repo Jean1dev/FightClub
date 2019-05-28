bc1e776f56ca984c429a84767482776fb7cabceb
Francisco <chicomazfilho@hotmail.com>
2019-05-27 21:14:33 -0300

    Correção

--- app/src/main/java/com/voador/guardeiro/flightclub/activities/BaseActivity.java
+++ app/src/main/java/com/voador/guardeiro/flightclub/activities/BaseActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class BaseActivity extends AppCompatActivity {
+abstract public class BaseActivity extends AppCompatActivity {
 
     protected void goTo(Class<?> toActivity) {
         startActivity(new Intent(this, toActivity));
